d6c475da97b72d6b922ce907b2db9bd6549db529
Sergey Chama <sergeychama24@gmail.com>
2024-11-15 19:52:56 +0300

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
-import {ServeStaticModule} from "@nestjs/serve-static";
-import {ConfigModule} from "@nestjs/config";
-import * as path from "node:path";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'node:path';
 
-import {configProvider} from "./app.config.provider";
+import { configProvider } from './app.config.provider';
 
 @Module({
   imports: [
-	ConfigModule.forRoot({
-          isGlobal: true,
-          cache: true
-      }),
-      // @todo: Добавьте раздачу статических файлов из public
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    // @todo: Добавьте раздачу статических файлов из public
   ],
   controllers: [],
   providers: [configProvider],
