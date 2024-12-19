84a22caf16b186ca22bb23208c7c706b12ee9e20
Sergey Chama <sergeychama24@gmail.com>
2024-12-19 07:52:10 +0300

    refactor repository

--- backend/src/repository/interfaces/films-repository.interface.ts
+++ backend/src/repository/interfaces/films-repository.interface.ts
@@ -1,9 +1,9 @@
-import { Film } from '../../films/entities/film.entity';
-import { Schedule } from '../../films/entities/schedule.entity';
+import { Films } from '../../films/entities/film.entity';
+import { Schedules } from '../../films/entities/schedule.entity';
 import { CreateOrderDto } from '../../order/dto/order.dto';
 
 export interface IFilmsRepository {
-  findAll(): Promise<{ total: number; items: Film[] }>;
-  findSchedule(id: string): Promise<{ total: number; items: Schedule[] }>;
+  findAll(): Promise<{ total: number; items: Films[] }>;
+  findSchedule(id: string): Promise<{ total: number; items: Schedules[] }>;
   createOrder(order: CreateOrderDto): Promise<{ message: string }>;
 }
